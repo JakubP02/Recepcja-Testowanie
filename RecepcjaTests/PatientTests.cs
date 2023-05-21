7907c2ca7cb82b4109f118732055afc638c05b74
KamilCieciura <kamil.cieciura2001@gmail.com>
2023-05-21 11:53:31 +0200

    Dodanie testów Xunit dla pacjentów

---
+++ RecepcjaTests/PatientTests.cs
@@ -0,0 +1,55 @@
+
+namespace RecepcjaTests
+{
+    public class PatientTests
+    {
+        /*
+    [Fact]
+        public void TestIfPeselIsInvalid()
+        {
+            DateTime birth = new DateTime(1980, 1, 1);
+            bool result = Form_add_patient.IsValidPesel("12345678901", birth, "M");
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void TestIfPeselIsValid()
+        {
+            DateTime birth = new DateTime(1980, 1, 1);
+            bool result = Form_add_patient.IsValidPesel("80010112340", birth, "K");
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void TestIfPeselHasWrongSex()
+        {
+            DateTime birth = new DateTime(1980, 1, 1);
+            bool result = Form_add_patient.IsValidPesel("80010112345", birth, "X");
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void TestIfPeselHasWrongBirthDate()
+        {
+            DateTime birth = new DateTime(2023, 4, 26);
+            bool result = Form_add_patient.IsValidPesel("80010112345", birth, "M");
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void TestIfPeselHasInvalidMonth()
+        {
+            DateTime birth = new DateTime(1980, 2, 1);
+            bool result = Form_add_patient.IsValidPesel("80020112345", birth, "M");
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void TestIfPeselHasInvalidDay()
+        {
+            DateTime birth = new DateTime(1980, 1, 2);
+            bool result = Form_add_patient.IsValidPesel("80010112345", birth, "M");
+            Assert.False(result);
+        }*/
+    }
+}
