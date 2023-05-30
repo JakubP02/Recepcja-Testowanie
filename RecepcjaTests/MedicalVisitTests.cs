4c4b9e5d71f737a237bd86d7a062a9a0e0bf6494
KamilCieciura <kamil.cieciura2001@gmail.com>
2023-05-31 01:18:39 +0200

    Dodanie testów Xunit dla wizyt

---
+++ RecepcjaTests/MedicalVisitTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Recepcja;
+
+namespace RecepcjaTests
+{
+    public class MedicalVisitTests
+    {
+        [Fact]
+        public void TestDoctorIdProperty()
+        {
+            MedicalVisit medicalVisit = new MedicalVisit();
+            int doctorId = 1234;
+            medicalVisit.Doctorid1 = doctorId;
+            Assert.Equal(doctorId, medicalVisit.Doctorid1);
+        }
+
+        [Fact]
+        public void TestPatientIdProperty()
+        {
+            MedicalVisit medicalVisit = new MedicalVisit();
+            int patientId = 5678;
+            medicalVisit.Patientid = patientId;
+            Assert.Equal(patientId, medicalVisit.Patientid);
+        }
+
+        [Fact]
+        public void TestDateProperty()
+        {
+            MedicalVisit medicalVisit = new MedicalVisit();
+            DateTime date = new DateTime(2023, 5, 30);
+            medicalVisit.Date1 = date;
+            Assert.Equal(date, medicalVisit.Date1);
+        }
+
+        [Fact]
+        public void TestTimeProperty()
+        {
+            MedicalVisit medicalVisit = new MedicalVisit();
+            TimeSpan time = new TimeSpan(14, 0, 0);
+            medicalVisit.Time = time;
+            Assert.Equal(time, medicalVisit.Time);
+        }
+    }
+}
